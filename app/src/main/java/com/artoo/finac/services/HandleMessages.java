9e46f2020c0b3984487dbbf8632741eb9f898e0c
Chandra Prakash <cprakashagr@gmail.com>
2015-11-02 19:55:26 +0530

    Messages Regular Expressions and Broadcasts Events

--- app/src/main/java/com/artoo/finac/services/HandleMessages.java
+++ app/src/main/java/com/artoo/finac/services/HandleMessages.java
@@ -4,23 +4,43 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
-import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HandleMessages extends BroadcastReceiver {
 
     //  DM
+    private static Context context;
     private final SmsManager sms = SmsManager.getDefault();
 
     private final static String TAG = "Finac SMS";
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
+    //  Singleton Implementation
+//    private static HandleMessages instance;
+//    private HandleMessages() {
+//    }
+//
+//    public static HandleMessages getInstance(Context c) {
+//
+//        context = c;
+//
+//        if (instance == null)
+//            instance = new HandleMessages();
+//
+//        return instance;
+//    }
 
+    @Override
+    public void onReceive(Context c, Intent intent) {
+
+        context = c;
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
         if (settings.getBoolean("readMessages",true)) {
 
@@ -40,13 +60,90 @@ public class HandleMessages extends BroadcastReceiver {
                         String senderNum = phoneNumber;
                         String message = currentMessage.getDisplayMessageBody();
 
-                        Log.i(TAG, "senderNum: " + senderNum + "; message: " + message);
-                        Toast.makeText(context, "senderNum: " + senderNum + ", message: " + message, Toast.LENGTH_SHORT).show();
+                        doParsingAndUpdateDB(message.toLowerCase(), senderNum);
                     }
                 }
             } catch (Exception e) {
                 Log.e("SmsReceiver", "Exception smsReceiver" + e);
             }
         }
+    }
+
+    private void doParsingAndUpdateDB(String message, String senderNum) {
+
+        double amt;
+        String type;
+
+        Pattern regEx  = Pattern.compile("[Ii][Nn][Rr](\\s*.\\s*\\d*)");
+        Pattern regEx2 = Pattern.compile("[rR][sS](\\s*.\\s*\\d*)");
+
+        Matcher matcher  = regEx.matcher(message);
+        Matcher matcher2 = regEx2.matcher(message);
+
+        if (message.contains("deposited") || message.contains("credited") || message.contains("cr")) {
+
+            type = "CR";
+        } else if (message.contains("withdrawn") || message.contains("debited") || message.contains("dr")) {
+
+            type = "DB";
+        }
+        else
+            //  No Transaction Tyoe Message found
+            return;
+
+        if (matcher.find()) {
+
+            String match = matcher.group();
+            match = match.replaceAll("([Ii][Nn][Rr])*([Rr][Ss])*","");
+            amt = Double.parseDouble(match);
+
+            //
+        } else if (matcher2.find()) {
+
+            String match = matcher2.group();
+            match = match.replaceAll("([Ii][Nn][Rr])*([Rr][Ss])*","");
+            amt = Double.parseDouble(match);
+
+        } else
+            return;
+
+        Log.d(TAG, "Fetched Amount" + amt + ", Type: " + type);
+
+        //  Time to Enter the DB and then broadcast and need to update the Preference too.
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = settings.edit();
+
+        if (type.equals("CR")){
+
+            Float val = settings.getFloat("credit", 0.0f);
+            editor.putFloat("credit", (float) (val+amt));
+        }
+        else {
+
+            Float val = settings.getFloat("debit", 0.0f);
+            editor.putFloat("debit", (float) (val+amt));
+        }
+
+        editor.apply();
+
+        SQLiteDatabase db = context.openOrCreateDatabase("finac",Context.MODE_PRIVATE,null);
+        String query;
+        if (type.equals("CR")) {
+
+            query = "INSERT INTO `transaction` (`amount`, `category`, `type`) VALUES(" + amt + ", '" + "Salary" + "', '" + type + "')";
+        } else {
+
+            query = "INSERT INTO `transaction` (`amount`, `category`, `type`) VALUES(" + amt + ", '" + "Other Sources" + "', '" + type + "')";
+        }
+        db.execSQL(query);
+        Log.d(TAG, 2 + "");
+
+        //  Need to update the
+
+        Intent intent = new Intent();
+        intent.setAction("COM_ARTOO_FINAC_ADDED_TRANSACTIONS");
+        Log.d(TAG, 3 + "");
+        context.sendBroadcast(intent);
+        Log.d(TAG, 4 + "");
     }
 }
